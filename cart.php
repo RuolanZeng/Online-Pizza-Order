dabe8f416ba2084fcad2e3f435613e9c46f0a665
Ruolan Zeng <rlzeng0616@gmail.com>
2018-04-29 20:26:02 -0500

    Update Nav

--- cart.php
+++ cart.php
@@ -8,6 +8,12 @@
 	<link href="http://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 	<link href="http://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="assets/css/htmleaf-demo.css">
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">   
+    <link href="assets/css/offcanvas.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 	<style type="text/css">
 		.box{
 			width: 20px;
@@ -26,7 +32,7 @@
 </head>
 <body>
 
-	<nav class="navbar navbar-fixed-top navbar-inverse" style="background-color:#ff471a;">
+	<nav class="navbar navbar-fixed-top navbar-inverse" style="background-color:#ff471a">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -35,22 +41,32 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">Pizza to Go</a>
+          <a class="navbar-brand" href="index.php" style="color: #ffffff; font-size:25px;"><i class="fas fa-utensils"></i> Pizza to Go</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="index.php">Home</a></li>
+          <!-- <ul class="nav navbar-nav">
+            <li><a href="#">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
-          </ul>
+          </ul> -->
           <ul id="upright" class="nav navbar-nav navbar-right">
             <li id="upright1" class="dropdown">
               <?php
                 session_start();
                 if (isset($_SESSION['username'])) {
-                  echo "<a href='logout.php'><b>Log out</b></a>";
+                  echo "<a href='logout.php' style='color: #ffffff;font-size:16px;'><b>Log out</b></a>";
                 }else{
-                  echo "<a href='signup.php'><b>Register</b></a>";
+                  echo "<a href='signup.php' style='color: #ffffff;font-size:16px;'><b>Register</b></a>";
+                }
+              ?>
+            </li>
+          </ul>
+          <ul id="upright" class="nav navbar-nav navbar-right">
+            <li id="upright1" class="dropdown">
+              <?php
+                session_start();
+                if (!isset($_SESSION['username'])) {
+                  echo "<a href='signin.php' style='color: #ffffff;font-size:16px;'><b>Log In</b></a>";
                 }
               ?>
             </li>
@@ -60,26 +76,29 @@
               <?php
                 session_start();
                 if (isset($_SESSION['username'])) {
-                  echo "<a href='cart.php'><b>Cart</b></a>";
-                }else{
-                  echo "<a href='signin.php'><b>Log In</b></a>";
-                }
-              ?>
-            </li>
-          </ul>
-          <ul id="upright" class="nav navbar-nav navbar-right">
-            <li id="upright1" class="dropdown">
-              <?php
-                session_start();
-                if (isset($_SESSION['username'])) {
-                  echo "<a href='editmenu.php'><b>".$_SESSION['username']."</b></a>";
+                  if (isset($_SESSION['admin'])) {
+                    echo "<a href='#' class='dropdown-toggle' data-toggle='dropdown' style='color: #ffffff;font-size:16px;'><b> Admin User: ".$_SESSION['username']."</b><span class='caret'></span></a>";
+                    echo "<ul id='drop' class='dropdown-menu'>";
+                    echo "<li><a role='menuitem' href='editmenu.php''>EDIT MENU</a></li>";
+                    echo "<li><a role='menuitem' href='cart.php''>CART</a></li>";
+                    echo "<li><a role='menuitem' href='orderinfo.php''>ORDER INFO</a></li>";
+                    echo "</ul>";
+                  }else{
+                    echo "<a href='#' class='dropdown-toggle' data-toggle='dropdown' style='color: #ffffff;font-size:16px;'><b> Normal User: ".$_SESSION['username']."</b><span class='caret'></span></a>";
+                    echo "<ul id='drop' class='dropdown-menu'>";
+                    echo "<li><a role='menuitem' href='cart.php'>CART</a></li>";
+                    echo "<li><a role='menuitem' href='orderinfo.php'>ORDER INFO</a></li>";
+                    echo "</ul>";
+                  } 
                 }
               ?>
             </li>
           </ul>
         </div><!-- /.nav-collapse -->
       </div><!-- /.container -->
-    </nav><!-- /.navbar --><br>
+    </nav><!-- /.navbar -->
+
+    <br>
 
 		
 		<div class="container">
--- index.php
+++ index.php
@@ -12,14 +12,17 @@
     <title>Pizza to Go</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-
-   
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">   
     <link href="assets/css/offcanvas.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+
   </head>
 
   <body>
-    <nav class="navbar navbar-fixed-top navbar-inverse">
+    <nav class="navbar navbar-fixed-top navbar-inverse" style="background-color:#ff471a">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -28,7 +31,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Pizza to Go</a>
+          <a class="navbar-brand" href="index.php" style="color: #ffffff; font-size:25px;"><i class="fas fa-utensils"></i> Pizza to Go</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <!-- <ul class="nav navbar-nav">
@@ -41,9 +44,19 @@
               <?php
                 session_start();
                 if (isset($_SESSION['username'])) {
-                  echo "<a href='logout.php'><b>Log out</b></a>";
+                  echo "<a href='logout.php' style='color: #ffffff;font-size:16px;'><b>Log out</b></a>";
                 }else{
-                  echo "<a href='signup.php'><b>Register</b></a>";
+                  echo "<a href='signup.php' style='color: #ffffff;font-size:16px;'><b>Register</b></a>";
+                }
+              ?>
+            </li>
+          </ul>
+          <ul id="upright" class="nav navbar-nav navbar-right">
+            <li id="upright1" class="dropdown">
+              <?php
+                session_start();
+                if (!isset($_SESSION['username'])) {
+                  echo "<a href='signin.php' style='color: #ffffff;font-size:16px;'><b>Log In</b></a>";
                 }
               ?>
             </li>
@@ -53,19 +66,20 @@
               <?php
                 session_start();
                 if (isset($_SESSION['username'])) {
-                  echo "<a href='cart.php'><b>Cart</b></a>";
-                }else{
-                  echo "<a href='signin.php'><b>Log In</b></a>";
-                }
-              ?>
-            </li>
-          </ul>
-          <ul id="upright" class="nav navbar-nav navbar-right">
-            <li id="upright1" class="dropdown">
-              <?php
-                session_start();
-                if (isset($_SESSION['username'])) {
-                  echo "<a href='editmenu.php'><b>".$_SESSION['username']."</b></a>";
+                  if (isset($_SESSION['admin'])) {
+                    echo "<a href='#' class='dropdown-toggle' data-toggle='dropdown' style='color: #ffffff;font-size:16px;'><b> Admin User: ".$_SESSION['username']."</b><span class='caret'></span></a>";
+                    echo "<ul id='drop' class='dropdown-menu'>";
+                    echo "<li><a role='menuitem' href='editmenu.php''>EDIT MENU</a></li>";
+                    echo "<li><a role='menuitem' href='cart.php''>CART</a></li>";
+                    echo "<li><a role='menuitem' href='orderinfo.php''>ORDER INFO</a></li>";
+                    echo "</ul>";
+                  }else{
+                    echo "<a href='#' class='dropdown-toggle' data-toggle='dropdown' style='color: #ffffff;font-size:16px;'><b> Normal User: ".$_SESSION['username']."</b><span class='caret'></span></a>";
+                    echo "<ul id='drop' class='dropdown-menu'>";
+                    echo "<li><a role='menuitem' href='cart.php'>CART</a></li>";
+                    echo "<li><a role='menuitem' href='orderinfo.php'>ORDER INFO</a></li>";
+                    echo "</ul>";
+                  } 
                 }
               ?>
             </li>
@@ -109,14 +123,35 @@
                 echo "<center><p class='description'>" . $description . "</p></center>";
                 echo "<center><p class='price'>$ " . $price . "</p></center>";
                 echo "<center><p class='stock'>Stock: " . $stock . "</p></center>";
-                echo "<center><p><a class='btn btn-default' href='addtocart.php?id={$id}' role='button'>Add to cart</a></p></center>";
+                if (isset($_SESSION['username'])) {
+                  echo "<center><p><a class='btn btn-default' href='addtocart.php?id={$id}' role='button'>Add to cart</a></p></center>";
+                }
                 echo "</div></div></div>";
               }
             ?>
 
-          </div>
-
         </div><!--/.col-xs-12.col-sm-9-->
+
+        <center><nav aria-label="Page navigation example">
+              <ul class="pagination">
+                <li class="page-item">
+                  <a class="page-link" href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                  <a class="page-link" href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div></center>
 
         <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
           <div class="list-group">
